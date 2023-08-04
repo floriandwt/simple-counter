abbb8d09ede519117788be4f7c401b635f42619f
flooorianflo <florian.kiem@hfg.design>
2023-08-04 13:16:41 +0200

    fix: minor fixes

--- src/index.jsx
+++ src/index.jsx
@@ -1,48 +1,19 @@
-/* @refresh reload */
 import { render } from "solid-js/web";
-import { Router, Route, Routes } from "@solidjs/router";
-// import { Router } from "solid-app-router";
-
-import "./index.css";
 import App from "./App";
 import Login from "./Login";
 import Edit from "./Edit";
-
-const root = document.getElementById("root");
-
-if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
-  throw new Error(
-    "Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?"
-  );
-}
+import { Router, Route, Routes } from "@solidjs/router";
+import "./index.css";
 
 render(
   () => (
-    <Router base="/">
-      <Routes>  
+    <Router>
+      <Routes>
         <Route path="/" component={App} />
         <Route path="/login" component={Login} />
         <Route path="/edit" component={Edit} />
-        <Route
-          path="*"
-          element={
-            <div class="text-white font-sans w-screen h-screen flex items-center justify-center text-4xl flex-col gap-16">
-              404
-              <div class="w-[90%] max-w-xs">
-                <button
-                  onClick={() => {
-                    window.location.href = "/";
-                  }}
-                  class="w-full rounded-2xl text-lg bg-gradient-to-t py-4 from-amber-600 to-amber-500 border border-amber-500 active:scale-95 transition-all"
-                >
-                  Zurück
-                </button>
-              </div>
-            </div>
-          }
-        />
       </Routes>
     </Router>
   ),
-  root
+  document.getElementById("root")
 );
